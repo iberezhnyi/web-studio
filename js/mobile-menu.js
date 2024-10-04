5c725dbbf94446de5f5be71b46bc41cdd3540575
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-10-04 15:03:58 +0300

    feature(mobile-menu): add js no-scroll

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,18 +8,22 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen)
     mobileMenu.classList.toggle('is-open')
 
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll'
-    bodyScrollLock[scrollLockMethod](document.body)
+    if (!isMenuOpen) {
+      document.body.style.overflow = 'hidden'
+      mobileMenu.style.overflow = 'auto'
+    } else {
+      document.body.style.overflow = ''
+      mobileMenu.style.overflow = ''
+    }
   }
 
   openMenuBtn.addEventListener('click', toggleMenu)
   closeMenuBtn.addEventListener('click', toggleMenu)
 
-  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return
     mobileMenu.classList.remove('is-open')
     openMenuBtn.setAttribute('aria-expanded', false)
-    bodyScrollLock.enableBodyScroll(document.body)
+    document.body.style.overflow = ''
   })
 })()
